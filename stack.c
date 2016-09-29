8697e145f8a5533ad50831ab8c122739cda8370e
ThibautLavenant <thibaut.lavenant@etu.univ_poitiers.fr>
2016-09-29 12:03:07 +0000

    Stack done

---
+++ stack.c
@@ -0,0 +1,110 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*   stack.c                                     #######     ###   ######     */
+/*                                               #######    ####   ##  ##     */
+/*   By: pi <thibaut.lavenant@gmail.com>         ##        #  ##   ##         */
+/*   Machine : pi                                ####     ######   ##         */
+/*                                               ####    ##   ##   ##         */
+/*   Created: 2016/09/29 08:54:13 by pi          ##     ##    ##   ##  ##     */
+/*   Updated: 2016/09/29 12:02:29 by pi          ##    ##     ##   ######.fr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "stack.h"
+
+//==============================================================================
+
+// Init a new stack
+t_stack		new_stack()
+{
+	return (NULL);
+}
+
+//==============================================================================
+
+// Stackup a data
+t_stack		stackup(t_stack stack, void *data)
+{
+	t_cell_simple	*elem;
+
+	elem = (t_cell_simple*)malloc(sizeof(t_cell_simple));
+	if (!elem)
+		return (NULL);
+	elem->data = data;
+	elem->next = stack;
+	return (elem);
+}
+
+//==============================================================================
+
+// Unstack a data
+t_stack		unstack(t_stack stack, void (*destroy)(void *))
+{
+	t_stack	ret_stack;
+
+	ret_stack = stack->next;
+	(*destroy)(stack->data);
+	free(stack);
+	return (ret_stack);
+}
+
+//==============================================================================
+
+// Read a data
+void	*stack_read(t_stack stack)
+{
+	return (stack->data);
+}
+
+//==============================================================================
+
+// Test if the stack is empty
+char	stack_empty(t_stack stack)
+{
+	return (stack == NULL);
+}
+
+//==============================================================================
+
+// Return the number of elem in the stack
+size_t	stack_size(t_stack stack)
+{
+	size_t	size;
+	
+	size = 0;
+	while (stack)
+	{
+		stack = stack->next;
+		size++;
+	}
+	return (size);
+}
+
+//==============================================================================
+
+// Destroy the whole stack
+void	stack_destroy(t_stack stack, void (*destroy)(void *))
+{
+	while (t_stack)
+		stack = unstack(stack, destroy);
+}
+
+//==============================================================================
+
+// Invert the pile
+t_stack	stack_invert(t_stack stack)
+{
+	t_cell_simple	*prev;
+	t_cell_simple	*curr;
+	t_cell_simple	*next;
+
+	prev = NULL;
+	curr = stack;
+	while (curr)
+	{
+		next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
+	}
+}
--- stack.h
+++ stack.h
@@ -6,7 +6,7 @@
 /*   Machine : pi                                ####     ######   ##         */
 /*                                               ####    ##   ##   ##         */
 /*   Created: 2016/09/29 08:22:54 by pi          ##     ##    ##   ##  ##     */
-/*   Updated: 2016/09/29 08:51:47 by pi          ##    ##     ##   ######.fr  */
+/*   Updated: 2016/09/29 12:02:26 by pi          ##    ##     ##   ######.fr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,15 @@
 
 	#include "cell.h"
 	
-	// A stack
 	typedef t_cell_simple		*t_stack;
+
+	t_stack		new_stack();
+	t_stack		stackup(t_stack stack, void *data);
+	t_stack		unstack(t_stack stack, void (*destroy)(void *));
+	void	*stack_read(t_stack stack);
+	char	stack_empty(t_stack stack);
+	size_t	stack_size(t_stack stack);
+	void	stack_destroy(t_stack stack, void (*destroy)(void *));
+	t_stack	stack_invert(t_stack stack);
 
 #endif
